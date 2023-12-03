211fb9132e6472b4a9b9bc7da4571cd3bccac0d9
Trisha Menon <trisha0menon@gmail.com>
2023-12-03 16:44:57 -0600

    utils.py and horizontal carving

--- seam_carving/seam_carving.py
+++ seam_carving/seam_carving.py
@@ -6,115 +6,105 @@ import random
 import numpy as np
 
 
-def findseam(image):
+def findseam(im):
     """
-    Find a seam running from top to bottom of image
+    Find a seam running from top to bottom of image or from left to right (once image is rotated)
     """
 
-    #1: convert to grayscale if necessary
-    grayscale = len(np.shape(image))==2
-    if grayscale:
-        gray = image.astype(np.uint8)
+    if len(np.shape(im)) == 2:
+        gray = im.astype(np.uint8)
     else:
-        gray = np.sum(image,axis=2).astype(np.uint8)
+        gray = np.sum(im,axis=2).astype(np.uint8)
     num_rows, num_cols = np.shape(gray)
 
 
-    #2: find the energy, or amount of interest in the image
-    # define energy based on pixel difference along a row
-
-    #assume edges wrap
-    #differences along rows
-    energy = np.copy(gray)
+    # find energy map
+    e_map = np.copy(gray)
     
-    energy[1:-1,1:-1] = (abs(gray[1:-1,1:-1]-gray[:-2,1:-1])+
-                     abs(gray[1:-1,1:-1]-gray[2:,1:-1])+
-                     abs(gray[1:-1,1:-1]-gray[1:-1,:-2])+
-                     abs(gray[1:-1,1:-1]-gray[1:-1,2:]))
+    e_map[1:-1,1:-1] = (abs(gray[1:-1,1:-1] - gray[:-2,1:-1]) + abs(gray[1:-1,1:-1] - gray[2:,1:-1]) +
+                     abs(gray[1:-1,1:-1] - gray[1:-1,:-2]) + abs(gray[1:-1,1:-1] - gray[1:-1,2:]))
 
-    energy[:,0] = (abs(gray[:,0]-gray[:,1])+
-                   abs(gray[:,0]-gray[:,-1]))
-    energy[:,-1] = (abs(gray[:,-1]-gray[:,-2])+
-                   abs(gray[:,-1]-gray[:,0]))
+    e_map[:,0] = (abs(gray[:,0] - gray[:,1]) + abs(gray[:,0] - gray[:,-1]))
+    e_map[:,-1] = (abs(gray[:,-1] - gray[:,-2]) + abs(gray[:,-1] - gray[:,0]))
 
-    energy[:,1:-1] = (abs(gray[:,1:-1]-gray[:,:-2])+
-                         abs(gray[:,1:-1]-gray[:,2:]))
+    e_map[:,1:-1] = (abs(gray[:,1:-1]-gray[:,:-2]) + abs(gray[:,1:-1]-gray[:,2:]))
 
-    #differences along columns
-    energy2 = np.copy(gray)
-    energy2[0,:] = (abs(gray[0,:]-gray[1,:])+
-                   abs(gray[0,:]-gray[-1,:]))
-    energy2[1,:] = (abs(gray[-1,:]-gray[-2,:])+
-                   abs(gray[-1,:]-gray[0,:]))
+    # differences along columns
+    e_map2 = np.copy(gray)
+    e_map2[0,:] = (abs(gray[0,:] - gray[1,:]) + abs(gray[0,:] - gray[-1,:]))
+    e_map2[1,:] = (abs(gray[-1,:] - gray[-2,:]) + abs(gray[-1,:] - gray[0,:]))
 
-    energy2[1:-1,:] = (abs(gray[1:-1,:]-gray[1:-1,:])+
-                         abs(gray[1:-1,:]-gray[1:-1,:]))
-    energy+=energy2
+    e_map2[1:-1,:] = (abs(gray[1:-1,:] - gray[1:-1,:]) + abs(gray[1:-1,:] - gray[1:-1,:]))
+    e_map += e_map2
 
 
-    #3: calculate energy paths
-    energypaths = np.zeros([num_rows,num_cols])
-    energypaths[0,:] = energy[0,:]
+    # calculate energy paths
+    e_paths = np.zeros([num_rows, num_cols])
+    e_paths[0,:] = energy[0,:]
 
     for row in np.arange(1,num_rows):
-        energypaths[row,0] = energy[row,0]+min(energypaths[row-1,:2])
-        energypaths[row,num_cols-1] = energy[row,num_cols-1]+min(energypaths[row-1,num_cols-2:])
+        e_paths[row,0] = energy[row,0]+min(e_paths[row-1,:2])
+        e_paths[row, num_cols-1] = energy[row, num_cols-1] + np.min(e_paths[row-1, num_cols-2:])
         
-        #left, center, or right
-        triples = np.zeros([num_cols-2,3])
-        triples[:,0] = energypaths[row-1,:num_cols-2]
-        triples[:,1] = energypaths[row-1,1:num_cols-1]
-        triples[:,2] = energypaths[row-1,2:]
-        energypaths[row,1:num_cols-1] = energy[row,1:num_cols-1]+np.min(triples,axis=1)       
+        channels = np.zeros([num_cols-2,3])
+        channels[:,0] = e_paths[row-1, :num_cols-2]
+        channels[:,1] = e_paths[row-1, 1:num_cols-1]
+        channels[:,2] = e_paths[row-1, 2:]
+        e_paths[row, 1:num_cols-1] = e_map[row, 1:num_cols-1] + np.min(channels, axis=1)       
     
-    #4: pick out the lowest energy path and construct the seam from it
+    #4: construct the seam from lowest e_path
     seam = np.zeros(num_rows).astype(int)
-    minimums = np.where(energypaths[-1,:]==energypaths[-1,:].min())[0]
-    seam[-1] = random.choice(minimums)
+    mins = np.where(e_paths[-1, :] == e_paths[-1, :].min())[0]
+    seam[-1] = random.choice(mins)
 
-    for row in np.arange(num_rows-2,-1,-1):
-        prevcol = seam[row+1]
-        if prevcol!=0 and prevcol!=num_cols-1:
-            if energypaths[row,prevcol-1]<energypaths[row,prevcol] and energypaths[row,prevcol-1]<energypaths[row,prevcol+1]:
-                seam[row] = prevcol-1
-            elif energypaths[row,prevcol+1]<energypaths[row,prevcol-1] and energypaths[row,prevcol+1]<energypaths[row,prevcol]:
-                seam[row] = prevcol+1
+    for row in np.arange(num_rows-2, -1, -1):
+        prev = seam[row+1]
+        if prev != 0 and prev != num_cols-1:
+            if e_paths[row, prev - 1] < e_paths[row, prev] and e_paths[row, prev - 1] < e_paths[row, prev + 1]:
+                seam[row] = prev - 1
+            elif e_paths[row, prev + 1] < e_paths[row, prev - 1] and e_paths[row, prev + 1] < e_paths[row, prev]:
+                seam[row] = prev + 1
             else:
-                seam[row] = prevcol  
-        if prevcol==0:
-            if energypaths[row,prevcol]<=energypaths[row,prevcol+1]:
-                seam[row] = prevcol    
+                seam[row] = prev  
+        if prev == 0:
+            if e_paths[row, prev]<=e_paths[row, prev + 1]:
+                seam[row] = prev    
             else:
-                seam[row] = prevcol+1
-        if prevcol==num_cols-1:
-            if energypaths[row,prevcol]<=energypaths[row,prevcol-1]:
-                seam[row] = prevcol    
+                seam[row] = prev + 1
+        if prev == num_cols-1:
+            if e_paths[row, prev]<=e_paths[row, prev - 1]:
+                seam[row] = prev    
             else:
-                seam[row] = prevcol-1
+                seam[row] = prev - 1
     
-    return seam, energypaths
+    return seam, e_paths
 
 
-def removeseam(image,seam):
+def removeseam(im,seam):
     """ 
-    Removes one seam from an image
+    Removes given seam from an image
     """
-    grayscale = len(np.shape(image))==2
     
-    if grayscale:
-        startrows, startcols = np.shape(image)
-        binarymask = np.ones(np.shape(image[:,:]),dtype=bool)
+    if len(np.shape(im)) == 2:
+        s_rows, s_cols = np.shape(im)
+        bin_mask = np.ones(np.shape(im[:,:]), dtype=bool)
     else:
-        startrows, startcols, colors = np.shape(image)
-        binarymask = np.ones(np.shape(image[:,:,0]),dtype=bool)
+        s_rows, s_cols, colors = np.shape(im)
+        bin_mask = np.ones(np.shape(im[:,:,0]), dtype=bool)
     
-    for row in range(startrows):
-        binarymask[row,seam[row]]=False
-    keep = image[binarymask]
+    for row in range(s_rows):
+        bin_mask[row, seam[row]]=False
+    keep = im[bin_mask]
     
-    if grayscale:
-        keep = keep.reshape(startrows,startcols-1)
+    if len(np.shape(im)) == 2:
+        keep = keep.reshape(s_rows, s_cols - 1)
     else:
-        keep = keep.reshape(startrows,startcols-1,colors)
+        keep = keep.reshape(s_rows, s_cols - 1, colors)
 
     return keep
+
+def rotate_im(im, num_rots):
+    # rotate once and then 3 more times to rotate back to normal
+    out = np.rot90(im, num_rots)
+    return out
+    
